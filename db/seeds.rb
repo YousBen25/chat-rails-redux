e1247c408511398feec1a9617a7632d722190699
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-01-15 18:28:50 +0100

    models seeds api/v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+puts "start seeding ..."
+names = ['general', 'paris', 'react']
+
+names.each do |name|
+  puts "seeding channels .."
+  Channel.create!(name: name)
+end
+
+5.times do
+  puts "seeding User .."
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    nickname: Faker::Internet.username
+    )
+  rand(2..4).times do
+    Message.create!(
+      content: Faker::TvShows::HowIMetYourMother.quote,
+      user: user,
+      channel: Channel.where("name = ?","general")[0]
+      )
+    end
+    rand(2..4).times do
+    Message.create!(
+      content: Faker::TvShows::HowIMetYourMother.quote,
+      user: user,
+      channel: Channel.where("name = ?","paris")[0]
+      )
+    end
+    rand(2..4).times do
+    Message.create!(
+      content: Faker::TvShows::HowIMetYourMother.quote,
+      user: user,
+      channel: Channel.where("name = ?","react")[0]
+      )
+    end
+end
+puts "end of the seeds ..."
